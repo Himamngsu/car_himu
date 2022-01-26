b9c7c8f69056de028f2bf45a05e38b9c0a5a738f
unknown <biswas.hima2@gmail.com>
2022-01-26 14:21:50 +0600

    car_a

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(function(){
+    'use strict';
+     // banner-part slider start
+     $('#banner_part').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: true,
+    });
+    
+    
+
+
+
+
+
+
+
+
+
+})
